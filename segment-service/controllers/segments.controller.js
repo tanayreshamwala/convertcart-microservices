c493168e64589fc7dfa3181733b15b73fc933560
IceCube <tanayreshamwala@gmail.com>
2025-11-04 15:57:19 +0530

    Fix: use PRODUCT_SERVICE_URL env variable for product-service calls

--- segment-service/controllers/segments.controller.js
+++ segment-service/controllers/segments.controller.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 import { parseRules } from "../util/ruleParser.js";
 import { evaluateRules } from "../util/ruleEvaluator.js";
+import { configDotenv } from "dotenv";
+configDotenv();
+
+const PRODUCT_SERVICE_URL =
+  process.env.PRODUCT_SERVICE_URL || "http://product-service:4000";
 
 export const evaluateSegmentRules = async (req, res) => {
   try {
@@ -13,7 +18,9 @@ export const evaluateSegmentRules = async (req, res) => {
     const rules = parseRules(rulesText);
 
     // Fetch products from product-service
-    const { data: products } = await axios.get("http://product-service:4000/products");
+    const { data: products } = await axios.get(
+      `${PRODUCT_SERVICE_URL}/products`
+    );
 
     // Apply rules
     const filtered = evaluateRules(products, rules);
