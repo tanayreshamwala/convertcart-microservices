07705e5ea704f7a32d72b7fe045134ee666d6821
IceCube <tanayreshamwala@gmail.com>
2025-11-04 18:14:16 +0530

    Changed ProductApi.js

--- frontend/src/api/ProductApi.js
+++ frontend/src/api/ProductApi.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+const PRODUCT_API_URL =
+  import.meta.env.VITE_PRODUCT_API_URL || "http://localhost:4000";
 
 export const fetchProducts = async () => {
-  const response = await axios.get("/products");
+  const response = await axios.get(`${PRODUCT_API_URL}/products`);
   return response.data;
 };
